060cc83c4ed7dcdeb424d0917cf921d7496760ba
Mac <mac@Macs-iMac.local>
2020-06-06 19:23:02 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/mkinnovativeapps/my_notes_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mkinnovativeapps.my_notes_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
